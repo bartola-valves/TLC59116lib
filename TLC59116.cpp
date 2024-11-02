0ac0ccd2c28ef1d67c69d1301d8eee1bffb966dc
Ale Moglia <valves@bartola.co.uk>
2024-11-02 19:14:34 +0000

    cleaned SDK conversion, take 1

--- TLC59116.cpp
+++ TLC59116.cpp
@@ -1,137 +1,245 @@
-#include "RB-TLC59116.h"
+#include "TLC59116.h"
+#include "hardware/i2c.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include <cstdio> // for printf
+
+/**
+ * @brief  TLC59116 class
+ *       Library for the TLC59116 I2C LED driver
+ *      https://www.ti.com/lit/ds/symlink/tlc59116.pdf
+ *
+ */
+
+/**
+ * Defines for the Class
+ *
+ */
+
+// I2C defines
+// This example will use I2C0 on GPIO0 (SDA) and GPIO1 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0 // I2C port number
+#define I2C_SDA 0     // SDA pin number
+#define I2C_SCL 1     // SCL pin number
 
 // Register Addresses
-const byte TLC_PWM_BASE = 0x02;
-const byte TLC_GROUP_PWM = 0x12;
-const byte TLC_GROUP_FREQ = 0x13;
-const byte TLC_LED_OUTPUT_ADDR = 0x14;
-const byte TLC_ERROR_FLAG1 = 0x1D;
-const byte TLC_ERROR_FLAG2 = 0x1E;
-const byte TLC_RESET = 0x6B;
+
+const uint8_t MODE1 = 0x00;      // Mode register 1. Read/Write
+const uint8_t MODE2 = 0x01;      // Mode register 2. Read/Write
+const uint8_t PWM0 = 0x02;       // PWM0 register - brightness for LED0. Read/Write
+const uint8_t PWM1 = 0x03;       // PWM1 register - brightness for LED1. Read/Write
+const uint8_t PWM2 = 0x04;       // PWM2 register - brightness for LED2. Read/Write
+const uint8_t PWM3 = 0x05;       // PWM3 register - brightness for LED3. Read/Write
+const uint8_t PWM4 = 0x06;       // PWM4 register - brightness for LED4. Read/Write
+const uint8_t PWM5 = 0x07;       // PWM5 register - brightness for LED5. Read/Write
+const uint8_t PWM6 = 0x08;       // PWM6 register - brightness for LED6. Read/Write
+const uint8_t PWM7 = 0x09;       // PWM7 register - brightness for LED7. Read/Write
+const uint8_t PWM8 = 0x0A;       // PWM8 register - brightness for LED8. Read/Write
+const uint8_t PWM9 = 0x0B;       // PWM9 register - brightness for LED9. Read/Write
+const uint8_t PWM10 = 0x0C;      // PWM10 register - brightness for LED10. Read/Write
+const uint8_t PWM11 = 0x0D;      // PWM11 register - brightness for LED11. Read/Write
+const uint8_t PWM12 = 0x0E;      // PWM12 register - brightness for LED12. Read/Write
+const uint8_t PWM13 = 0x0F;      // PWM13 register - brightness for LED13. Read/Write
+const uint8_t PWM14 = 0x10;      // PWM14 register - brightness for LED14. Read/Write
+const uint8_t PWM15 = 0x11;      // PWM15 register - brightness for LED15. Read/Write
+const uint8_t GRPPWM = 0x12;     // Group PWM brightness -duty cycle. Read/Write
+const uint8_t GRPFREQ = 0x13;    // Group frequency. Read/Write
+const uint8_t LEDOUT0 = 0x14;    // LED output state register 0. Read/Write
+const uint8_t LEDOUT1 = 0x15;    // LED output state register 1. Read/Write
+const uint8_t LEDOUT2 = 0x16;    // LED output state register 2. Read/Write
+const uint8_t LEDOUT3 = 0x17;    // LED output state register 3. Read/Write
+const uint8_t SUBADR1 = 0x18;    // I2C-bus subaddress 1. Read/Write
+const uint8_t SUBADR2 = 0x19;    // I2C-bus subaddress 2. Read/Write
+const uint8_t SUBADR3 = 0x1A;    // I2C-bus subaddress 3. Read/Write
+const uint8_t ALLCALLADR = 0x1B; // LED All Call I2C-bus address. Read/Write
+const uint8_t IREF = 0x1C;       // IREF configuration register. Read/Write
+const uint8_t ERRFLAG1 = 0x1D;   // Error flag 1. Read only
+const uint8_t ERRFLAG2 = 0x1E;   // Error flag 2. Read only
+
+const uint8_t TLC_RESET = 0x6B; // Reset all TLC59116s
 
 // Possible LED Output States
-const byte LED_OFF = 0x00;
-const byte LED_ON = 0x01;
-const byte LED_PWM = 0x02;
-const byte LED_GROUP = 0x03;
+const uint8_t LED_OFF = 0x00;   // LED off . Default state of the LED during power up
+const uint8_t LED_ON = 0x01;    // LED on. LED is on at full brightness
+const uint8_t LED_PWM = 0x02;   // LED PWM. LED driver is controlled by the PWM register
+const uint8_t LED_GROUP = 0x03; // LED group. LED driver is controlled by the group PWM and group FREQ registers (blinking)
 
 //<<constructor>> setup the TLC59116
-TLC59116::TLC59116(int address, byte resetPin) : TLC_ADDR(address), RESET(resetPin) {
-  digitalWrite(RESET, HIGH);
-  boolean groupMode = 0; // Group dimming(0) or blinking(1)
-  byte LEDOUT[4] = { 0x00, 0x00, 0x00, 0x00 }; // Keep track of channel output modes
-  unsigned int ERR_FLAG = 0xFFFF;
+TLC59116::TLC59116(uint8_t address, uint8_t resetPin)
+    : TLC_ADDR(address),
+      RESET(resetPin),
+      ERR_FLAG(0),
+      groupMode(0)
+{
+  gpio_init(RESET); // initialize the RESET pin. Output direction
+  gpio_set_dir(RESET, GPIO_OUT);
+  gpio_put(RESET, 1); // Reset is active low. Set it high to start with.
+
+  // Initialise the class variables
+  // I2C address of the TLC59116
+  groupMode = 0; // Group dimming(0) or blinking(1)
+  LEDOUT[0] = 0x00;
+  LEDOUT[1] = 0x00;
+  LEDOUT[2] = 0x00;
+  LEDOUT[3] = 0x00;  // LED output drive state registers
+  ERR_FLAG = 0xFFFF; // Error flag
 }
-  
+
 //<<destructor>>
 TLC59116::~TLC59116() {}
 
-void TLC59116::writeToDevice(byte reg, byte newLEDOUT)
+/**
+ * @brief  Initialize the I2C bus to interface with the TLC59116
+ *
+ * I2C Initialisation. Using it at 400Khz and defined I2C port, SDA and SCL pins.
+ * Pull-up resistors are enabled on the SDA and SCL lines.
+ * Finally enable the TLC59116 driver.
+ * @param  None
+ * @retval None
+ *
+ */
+
+void TLC59116::begin()
 {
-  Wire.beginTransmission(TLC_ADDR);
-  Wire.write(reg);
-  Wire.write(newLEDOUT);
-  Wire.endTransmission();
+
+  // use the defines for the I2C port and pins
+  i2c_init(I2C_PORT, 400 * 1000);
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
+
+  // Enable the pull-up resistors on the I2C bus
+  gpio_pull_up(I2C_SDA); // Enable the pull-up resistor on the SDA line
+  gpio_pull_up(I2C_SCL); // Enable the pull-up resistor on the SCL line
+  enableTLC();           // Start up the driver in a fresh state
 }
 
-byte TLC59116::readFromDevice(byte reg)
+// Write a value to a LED register of the TLC59116.
+
+void TLC59116::writeToDevice(uint8_t reg, uint8_t newLEDOUT)
 {
-  Wire.beginTransmission(TLC_ADDR);
-  Wire.write(reg);
-  Wire.requestFrom(TLC_ADDR, 1);
-  uint8_t result = Wire.read();
-  Wire.endTransmission();
+  uint8_t data[2] = {reg, newLEDOUT};
+  i2c_write_blocking(i2c0, TLC_ADDR, data, 2, false);
+}
+
+uint8_t TLC59116::readFromDevice(uint8_t reg)
+{
+  uint8_t result;
+  i2c_write_blocking(i2c0, TLC_ADDR, &reg, 1, true); // true to keep master control of bus
+  i2c_read_blocking(i2c0, TLC_ADDR, &result, 1, false);
   return result;
 }
 
 // Datasheet 9.5.6
 // --------------------------------------------------------------------
 // When modifying a channel output, maintain all other channel outputs.
-byte TLC59116::modifyLEDOutputState(byte LED, byte state) {
-  byte pos = ((LED % 4) * 2);
-  byte regLoc = LED / 4;
-  byte mask = 0x03 << pos;
-  byte newLEDOUT;
+// --------------------------------------------------------------------
 
-  newLEDOUT = LEDOUT[regLoc] | mask;
-  newLEDOUT = LEDOUT[regLoc] & ~mask;
-  if (state != 0x00) newLEDOUT = LEDOUT[regLoc] | (state << pos);
-  LEDOUT[regLoc] = newLEDOUT;
+uint8_t TLC59116::modifyLEDOutputState(uint8_t LED, uint8_t state)
+{
+  uint8_t pos = ((LED % 4) * 2); // Position of the LED in the register. Each LED takes 2 bits.
+  uint8_t regLoc = LED / 4;      // Register location of the LED. Each register holds 4 LEDs.
+  uint8_t mask = 0x03 << pos;    // Mask to clear the LED bits in the register.
+  uint8_t newLEDOUT;             // New LED output state register value.
 
-  writeToDevice(TLC_LED_OUTPUT_ADDR + regLoc, newLEDOUT);
+  newLEDOUT = LEDOUT[regLoc] | mask;  // Clear the LED bits in the register.
+  newLEDOUT = LEDOUT[regLoc] & ~mask; // Clear the LED bits in the register.
+  if (state != 0x00)
+    newLEDOUT = LEDOUT[regLoc] | (state << pos); // Set the LED bits in the register.
+
+  LEDOUT[regLoc] = newLEDOUT;                 // Update the LED output state register value.
+  writeToDevice(LEDOUT0 + regLoc, newLEDOUT); // Write the new LED output state register value to the TLC59116.
+  return newLEDOUT;
 }
 
-void TLC59116::LEDOff(byte LED) {
-  modifyLEDOutputState(LED, LED_OFF);
-}
-
-void TLC59116::LEDOn(byte LED) {
+void TLC59116::LEDOn(uint8_t LED)
+{
   modifyLEDOutputState(LED, LED_ON);
 }
 
-void TLC59116::LEDPWM(byte LED) {
+void TLC59116::LEDOff(uint8_t LED)
+{
+  modifyLEDOutputState(LED, LED_OFF);
+}
+
+void TLC59116::LEDPWM(uint8_t LED)
+{
   setPWM(LED, 255);
   modifyLEDOutputState(LED, LED_PWM);
 }
 
-void TLC59116::LEDGroup(byte LED) {
+void TLC59116::LEDGroup(uint8_t LED)
+{
   setPWM(LED, 255);
   modifyLEDOutputState(LED, LED_GROUP);
 }
 // --------------------------------------------------------------------
 
 // Datasheet 9.5.3
-void TLC59116::setPWM(byte pin, byte duty) {
-  writeToDevice(TLC_PWM_BASE + pin, duty);
+void TLC59116::setPWM(uint8_t pin, uint8_t duty)
+{
+  writeToDevice(PWM0 + pin, duty);
 }
 
 // Datasheet 9.5.4
-void TLC59116::setGroupPWM(byte duty) {
-  if (groupMode == 1) {
+void TLC59116::setGroupPWM(uint8_t duty)
+{
+  if (groupMode == 1)
+  {
     writeToDevice(0x01, 0x00);
     groupMode = 0;
   }
-  writeToDevice(TLC_GROUP_PWM, duty);
+  writeToDevice(GRPPWM, duty);
 }
 
-void TLC59116::setGroupBlink(byte freq, byte duty) {
+void TLC59116::setGroupBlink(uint8_t freq, uint8_t duty)
+{
   setGroupPWM(duty);
-  if (groupMode == 0) {
+  if (groupMode == 0)
+  {
     writeToDevice(0x01, 0x20);
     groupMode = 1;
   }
 
-  writeToDevice(TLC_GROUP_FREQ, freq);
+  writeToDevice(GRPFREQ, freq);
 }
 
 // Datasheet 9.5.2
-void TLC59116::clearErrors() {
+void TLC59116::clearErrors()
+{
   writeToDevice(0x01, 0x80);
   writeToDevice(0x01, 0x00);
 }
 
 // Datasheet 9.3.2 & 9.5.10
-boolean TLC59116::checkErrors() {
-  byte flag1 = readFromDevice(TLC_ERROR_FLAG1);
-  byte flag2 = readFromDevice(TLC_ERROR_FLAG2);
-  Serial.println(flag1);
-  Serial.println(flag2);
+bool TLC59116::checkErrors()
+{
+  uint8_t flag1 = readFromDevice(ERRFLAG1);
+  uint8_t flag2 = readFromDevice(ERRFLAG2);
+  printf("Flag1: 0x%02X\n", flag1);
+  printf("Flag2: 0x%02X\n", flag2);
   ERR_FLAG = (flag2 << 8) + flag1;
 
-  if (ERR_FLAG == 0xFFFF) return false;
+  if (ERR_FLAG == 0xFFFF)
+    return false;
   return true;
-   
 }
 
-void TLC59116::reportErrors(){
-  if (ERR_FLAG == 0xFFFF) Serial.println("No errors detected.");
-  else {
-    Serial.println(ERR_FLAG, HEX);
-   for (byte i = 0; i < 16; i++) {
-    if (!((ERR_FLAG >> i) & 0x1)) {
-      Serial.print("Channel ");
-      Serial.print(i);
-      Serial.println(" overtemperature or open circuit.");
+void TLC59116::reportErrors()
+{
+  if (ERR_FLAG == 0xFFFF)
+  {
+    printf("No errors detected.\n");
+  }
+
+  else
+  {
+    printf("Error Flag: 0x%04X\n", ERR_FLAG);
+    for (uint8_t i = 0; i < 16; i++)
+    {
+      if (!((ERR_FLAG >> i) & 0x1))
+      {
+        printf("Channel %d overtemperature or open circuit.\n", i);
       }
     }
   }
@@ -139,25 +247,30 @@ void TLC59116::reportErrors(){
 }
 
 // Start up the driver in a fresh state
-void TLC59116::enableTLC() {
+void TLC59116::enableTLC()
+{
   writeToDevice(0x00, 0x80);
 }
 
 // Reset the driver either by the RESET pin, and if it was not defined just turn off all the LEDs.
-void TLC59116::resetDriver(){
-  for(int i=0; i<4; i++){
+void TLC59116::resetDriver()
+{
+  for (int i = 0; i < 4; i++)
+  {
     LEDOUT[i] = 0x00;
   }
 
-  digitalWrite(RESET, LOW);
-  delay(1); // Allow a microsecond for the device to power down.
-  digitalWrite(RESET, HIGH);
+  gpio_put(RESET, 0); // digitalWrite(RESET, LOW);
+  sleep_ms(1);        // delay(1); // Allow a millisecond for the device to power down.
+  gpio_put(RESET, 1); // digitalWrite(RESET, HIGH);
   enableTLC();
 }
 
 // Nice function to turn off all LED channels.
-void TLC59116::turnOffAllLEDs(){
-  for(byte i=0; i<16; i++){
+void TLC59116::turnOffAllLEDs()
+{
+  for (uint8_t i = 0; i < 16; i++)
+  {
     LEDOff(i);
   }
 }
@@ -165,12 +278,11 @@ void TLC59116::turnOffAllLEDs(){
 // Datasheet 9.3.5
 void TLC59116::resetAllTLCs()
 {
-  for (byte i = 0; i < 4; i++) LEDOUT[i] = 0x00;
+  for (uint8_t i = 0; i < 4; i++)
+    LEDOUT[i] = 0x00;
 
-  Wire.beginTransmission(0x6B);
-  Wire.write(0xA5);
-  Wire.write(0x5A);
-  Wire.endTransmission();
+  uint8_t data[2] = {0xA5, 0x5A};                 // Magic reset sequence
+  i2c_write_blocking(i2c0, 0x6B, data, 2, false); // Reset all TLC59116s
 
-  enableTLC();
+  enableTLC(); // Start up the driver in a fresh state
 }
--- TLC59116.h
+++ TLC59116.h
@@ -1,41 +1,67 @@
-#pragma once
-
 #ifndef TLC59116_H
 #define TLC59116_H
 
-#include <arduino.h>
-#include <Wire.h>
+#pragma once
+#include <stdint.h>
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
 
-class TLC59116 {
-  int TLC_ADDR;
-  byte RESET;
-  
-  public:
-  	TLC59116(int address, byte resetPin);
-  	~TLC59116();
-  
-  	void LEDOff(byte LED);
-  	void LEDOn(byte LED);
-  	void LEDPWM(byte LED);
-  	void LEDGroup(byte LED);
-  	void setPWM(byte LED, byte duty);
-  	void setGroupPWM(byte duty);
-  	void setGroupBlink(byte freq, byte duty);
-  	void clearErrors();
-  	boolean checkErrors();
-    void reportErrors();
-  	void enableTLC();
-    void resetDriver();
-    void turnOffAllLEDs();
-  	void resetAllTLCs();
-  
-  private:
-  	void writeToDevice(byte reg, byte val);
-  	byte readFromDevice(byte reg);
-  	byte modifyLEDOutputState(byte LED, byte state);    
-    boolean groupMode;
-    byte LEDOUT[4];
-    unsigned int ERR_FLAG;
+/**
+ *  @brief  TLC59116 class
+ * 			Library for the TLC59116 I2C LED driver
+ * 			https://www.ti.com/lit/ds/symlink/tlc59116.pdf
+ *
+ *
+ * @param  i2c_port: I2C port number
+ * @param  sda_pin: SDA pin number
+ * @param  scl_pin: SCL pin number
+ *
+ *
+ */
+class TLC59116
+{
+public:
+	TLC59116(uint8_t address, uint8_t resetPin); // Constructor. Set the I2C address and the RESET pin
+
+	void begin();								 // Initialize the I2C bus to interface with the TLC59116
+	void setPWM(uint8_t channel, uint8_t value); // Set the PWM value of a single channel
+	// void setAllPWM(uint8_t value);					 // Set the PWM value of all channels
+	// void setChannel(uint8_t channel, uint8_t value); // Set the output value of a single channel
+	// void setAllChannels(uint8_t value);				 // Set the output value of all channels
+	// void update();									 // Update the output values of the channels
+
+	void LEDOn(uint8_t LED);								  // Turn on a single LED
+	void LEDOff(uint8_t LED);								  // Turn off a single LED
+	void LEDPWM(uint8_t LED);								  // Set a single LED to PWM mode
+	void LEDGroup(uint8_t LED);								  // Set a single LED to group mode
+	void setGroupBlink(uint8_t freq, uint8_t duty);			  // Set the blink frequency and duty cycle of all group mode LEDs
+	void clearErrors();										  // Clear the error flags of the TLC59116
+	void reportErrors();									  // Report the error flags of the TLC59116 via the serial port
+	bool checkErrors();										  // Check for errors in the TLC59116. Use the reportErrors() function to print them
+	void resetDriver();										  // Reset the TLC59116. Also enables it at the end
+	void turnOffAllLEDs();									  // Turn off all LEDs
+	void resetAllTLCs();									  // Reset all TLC59116s
+	void enableTLC();										  // Enable the TLC59116. First thing to do.
+	void setPWM(uint8_t pin, uint8_t duty);					  // Set the PWM value of a single LED
+	void setGroupPWM(uint8_t duty);							  // Set the PWM value of all group mode LEDs
+	void writeToDevice(uint8_t reg, uint8_t newLEDOUT);		  // Write a value to a register of the TLC59116
+	uint8_t readFromDevice(uint8_t reg);					  // Read a value from a register of the TLC59116
+	uint8_t modifyLEDOutputState(uint8_t LED, uint8_t state); // Modify the output state of a single LED
+	// destructors
+	~TLC59116(); // Destructor
+private:
+	uint8_t i2c_port; // I2C port number
+	uint8_t sda_pin;  // SDA pin number
+	uint8_t scl_pin;  // SCL pin number
+
+	// void writeRegister(uint8_t reg, uint8_t value); // Write a value to a register of the TLC59116.
+	// uint8_t readRegister(uint8_t reg);				// Read a value from a register of the TLC59116.
+
+	uint8_t groupMode; // Group mode flag
+	uint8_t LEDOUT[4]; // LED output drive state registers
+	uint16_t ERR_FLAG; // Error flag
+	uint8_t TLC_ADDR;  // I2C address of the TLC59116
+	uint8_t RESET;	   // RESET pin
 };
 
-#endif // !TLC59116_H
+#endif // TLC59116_H
